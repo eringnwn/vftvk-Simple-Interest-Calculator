dfb58efc1a8b20ae16d8c9b71220b9059c8a8c24
eringnwn <105049956+eringnwn@users.noreply.github.com>
2022-05-07 23:12:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function updaterate()
+{
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = `${rateval}%`; 
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let amount = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let year = document.getElementById("years").value;
+
+    let interest = ((amount*rate)/100)*year;
+    year = new Date().getFullYear() + year;
+    document.getElementById("result").innerHTML = `If you deposit <mark>${amount}</mark>, <br>
+    at an interest rate of <mark>${rate}%</mark>. <br>
+    You will receive an amount of <mark>${interest}</mark> <br>
+    in the year of <mark>${year}</mark>`
 }
         
